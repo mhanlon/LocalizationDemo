4abb3d2e7dcde7859b05ea1469282e745cb965c8
Matt Hanlon <mhanlon@gmail.com>
2020-06-29 23:37:24 +0100

    Initial Commit

---
+++ LocalizationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizationDemo
+//
+//  Created by Matthew Hanlon on 6/29/20.
+//  Copyright © 2020 The Code Hub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
